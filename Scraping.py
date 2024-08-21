3fe0bc03010c6198af4ec720f05ec7875af3d4b0
Lockps <Rulic1137@gmail.com>
2024-08-21 20:11:37 +0700

    Find error

--- Scraping.py
+++ Scraping.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 import Parameter
 
@@ -8,6 +9,23 @@ def main():
     website = env.website
 
     print(website)
+    result = requests.get(website)
+
+    # Ensure correct encoding is set
+    result.encoding = 'utf-8'
+
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    strsoup = str(soup)
+    # print(type(str(soup)))
+
+    # print(soup)
+
+    file_path = "./test.html"
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(strsoup)
 
 
 if __name__ == "__main__":
